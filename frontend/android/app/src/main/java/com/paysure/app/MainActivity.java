d1eb30e216e87e7e71d993a701c6336c7d64e71b
AkashK0907 <akashk79026@gmail.com>
2026-04-30 09:15:40 +0530

    Push all remaining changes including Android app updates

--- frontend/android/app/src/main/java/com/paysure/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/paysure/app/MainActivity.java
@@ -1,11 +1,28 @@
 package com.paysure.app;
 
 import android.os.Bundle;
+import android.os.Build;
+import android.view.WindowManager;
+import android.view.View;
+import android.graphics.Color;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Draw behind the notch
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+        
+        // Make status bar transparent and draw app under it
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+    }
     @Override
     public void onStart() {
         super.onStart();
